eee82d8aadffd211bfaf7113d84ebef9ed93720a
bestware <49852633+ksh1t1z@users.noreply.github.com>
2021-03-31 12:47:05 +0545

    creation

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+import tkn as t
+import search as s
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('$'):
+        opcodes = s.search(t.file, message.content[1:].upper())
+        await message.channel.send(opcodes)
+
+
+client.run(t.TOKEN)
---
+++ search.py
@@ -0,0 +1,17 @@
+def search(file_name, string_to_search):
+    with open(file_name, 'r') as read_obj:
+        for line in read_obj:
+            if string_to_search in line:
+                return line
+        return 'Does not match!'
+
+
+def take_input():
+    while 1:
+        to_search = input("Enter string to search: ").upper()
+        x = search("opcodes8085.txt", to_search)
+        print(x)
+
+
+if __name__ == '__main__':
+    take_input()
